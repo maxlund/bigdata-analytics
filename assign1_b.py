f44343c9109b31a3234102f7aede66342f2e269c
Max Lund <maxlun@gmail.com>
2018-04-25 13:23:50 +0200

    Update assign1_b.py

--- assign1_b.py
+++ assign1_b.py
@@ -1,18 +1,20 @@
-with open("/nfshome/hadoop_examples/shared_data/temperatures-big.csv") as f:
-    data = [x.strip('\n') for x in f.readlines()]
+import csv
 
-data = [x.split(';') for x in data]
-years = dict()
+with open("/nfshome/hadoop_examples/shared_data/temperatures-big.csv", "rb") as f:
+    data = csv.reader(f, delimiter=";")
 
-for entry in data:
-    key = entry[1][0:4]
-    value = float(entry[3])
-    if key in years:
-        if years[key] < value:
-            years[key] = value
-    else:
-        years[key] = value
+    years = dict()
 
-with open("temps_no_spark.txt", "wb") as f:
-    for k, i in years.items():
-        f.write(k + ": " i + "\n")
+    for entry in data:
+        key = entry[1][0:4]
+        value = float(entry[3])
+        if key in years:
+            if years[key] < value:
+                years[key] = value
+            else:
+                years[key] = value
+
+# need to save?
+# with open("maxtemps_no_spark.txt", "wb") as f:
+#     for k, i in years.items():
+#         f.write(k + ": " i + "\n")
