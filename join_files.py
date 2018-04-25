749d353e55ed36b5b4b1e6def08fd8aaf6be4e10
Max Lund <maxlun@gmail.com>
2018-04-25 13:12:35 +0200

    Add files via upload

---
+++ join_files.py
@@ -0,0 +1,11 @@
+import os, sys
+
+files = os.listdir(sys.argv[1])
+data = list()
+for fn in files:
+    with open(sys.argv[1]+'/'+fn, 'rb') as input_file:
+        data.extend(input_file.readlines())
+
+with open(sys.argv[2], 'wb') as output_file:
+    for line in data:
+        output_file.write(line)
