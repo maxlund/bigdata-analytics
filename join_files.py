e7c2a7a6ca4982fe0fb47019c76f0226276c80ba
Max Lund <maxlun@gmail.com>
2018-04-26 10:39:58 +0200

    Update join_files.py

--- join_files.py
+++ join_files.py
@@ -1,12 +1,11 @@
-import os, sys, csv
+import os, sys
 
 files = os.listdir(sys.argv[1])
 data = list()
 for fn in files:
     with open(sys.argv[1]+'/'+fn, 'rb') as input_file:
         data.extend(input_file.readlines())
-
-with open(sys.argv[2], 'wb') as csv_file:
-    mycsv = csv.writer(csv_file, delimiter=',')
-    for row in data:
-        mycsv.writerow(row)
+        
+with open(sys.argv[2], 'wb') as output_file:
+    for line in data:
+        output_file.write(line)
