a4dce4012787cddd3dd5c4c2fff86dea6900309a
Max Lund <maxlun@gmail.com>
2018-05-02 14:46:50 +0200

    Add files via upload

--- lab1/assign6.py
+++ lab1/assign6.py
@@ -5,21 +5,18 @@ sc = SparkContext(appName = "assign6")
 
 # Assignment 6
 
-# temps_file lines are in format:
-# Station number;Date;Time;Air temperature;Quality
+# tempss_file lines are in format:
+# Station number;Date;Time;Air tempserature;Quality
 # 102170;2013-11-01;06:00:00;6.8;G
 
 # precip_file lines are in format:
 # Station number, Date, Time, Precipitation, Quality
 # 103100;1995-08-01;00:00:00;0.0;Y
 
-# ---------> READ THIS
-### TODO : WOPS! It's supposed to be TEMPERATURE NOT PRECIPITATION!!! ####
-
-precip_file = sc.textFile("data/precipitation-readings.csv")
+temps_file = sc.textFile("data/temperature-readings.csv")
 station_file = sc.textFile("data/stations-Ostergotland.csv")
 # split each line
-precip_lines = precip_file.map(lambda line: line.split(";"))
+temps_lines = temps_file.map(lambda line: line.split(";"))
 station_lines = station_file.map(lambda line: line.split(";"))
 
 # get only the Ostergotland station numbers
@@ -29,32 +26,51 @@ station_numbers = station_lines.map(lambda line: line[0])
 station_numbers = sc.broadcast(station_numbers.collect())
 
 # filter out only the ostergotland stations
-precip_lines = precip_lines.filter(lamdba x: x[0][0] in station_numbers.value)
+temps_lines = temps_lines.filter(lambda x: x[0] in station_numbers.value)
 
-# filter out years 1950-2014 and create key-value pairs of (year+month, precip)
-precip_1950_2014 = precip_lines.filter(lambda line: int(line[1][0:4]) <= 1950 and  int(line[1][0:4]) >= 2014)
-precip_1950_2014 = precip_1950_2014.map(lambda line: (line[1][0:7], float(line[3])))
+# print('temp lines i station numbs:', temp_lines.collect()[:10])
 
-# filter out years 1950-1980 and create key-value pairs of (month, precip)
-precip_1950_1980 = precip_lines.filter(lambda line: int(line[1][0:4]) <= 1950 and  int(line[1][0:4]) >= 1980)
-precip_1950_1980 = precip_1950_1980.map(lambda line: (line[1][5:7], float(line[3])))
+# filter out years 1950-2014 and create key-value pairs of (year+month, temps)
+temps_1950_2014 = temps_lines.filter(lambda line: int(line[1][0:4]) >= 1950 and  int(line[1][0:4]) <= 2014)
+temps_1950_2014 = temps_1950_2014.map(lambda line: (line[1][0:7], float(line[3])))
 
-# get the average precipitation by station number for every month of every year in 1950-2014
-sum_count_1950_2014 = precip_1950_2014.combineByKey(lambda value: (value, 1),
+# bla = temps_1950_2014.collect()
+# print("temps 1950-2014", bla[:10])
+
+# filter out years 1950-1980 and create key-value pairs of (month, temps)
+temps_1950_1980 = temps_lines.filter(lambda line: int(line[1][0:4]) >= 1950 and  int(line[1][0:4]) <= 1980)
+temps_1950_1980 = temps_1950_1980.map(lambda line: (line[1][5:7], float(line[3])))
+
+
+# bla2 = temps_1950_1980.collect()
+# print("temps 1950-1980", bla2[:10])
+
+# get the average tempsitation by station number for every month of every year in 1950-2014
+sum_count_1950_2014 = temps_1950_2014.combineByKey(lambda value: (value, 1),
                                                     lambda x, value: (x[0] + value, x[1] + 1),
                                                     lambda x, y: (x[0] + y[0], x[1] + y[1]))
-average_precip_1950_2014 = sum_count_1950_2014.map(lambda (key, (total_sum, count)): (key, total_sum / count))
+average_temps_1950_2014 = sum_count_1950_2014.map(lambda (key, (total_sum, count)): (key, total_sum / count))
 
-# get the average monthly precipitation by station number over all years 1950-1980
-sum_count_1950_1980 = precip_1950_1980.combineByKey(lambda value: (value, 1),
+# avg1 = average_temps_1950_2014.collect()
+# print("avg 1950-2014", avg1[:10])
+
+# get the average monthly tempsitation by station number over all years 1950-1980
+sum_count_1950_1980 = temps_1950_1980.combineByKey(lambda value: (value, 1),
                                                     lambda x, value: (x[0] + value, x[1] + 1),
                                                     lambda x, y: (x[0] + y[0], x[1] + y[1]))
-average_precip_1950_1980 = sum_count_1950_1980.map(lambda (key, (total_sum, count)): (key, total_sum / count))
+average_temps_1950_1980 = sum_count_1950_1980.map(lambda (key, (total_sum, count)): (key, total_sum / count))
 
-# broadcast the total average over 1950-1980
-average_var = sc.broadcast(sorted(average_precip_1950_1980.collect()))
 
-# TODO : since broadcast var is sorted, we can index the list by slicing the month and getting the correct one
-precip_differences = average_precip_1950_2014.map(lambda x: 
+# avg2 = average_temps_1950_1980.collect()
+# print("avg 1950-1980", avg2[:10])
 
-average_precip.saveAsTextFile("ostergotland_avg_precip")
+# create a dictionary/hasmap broadcast variable of the total average over 1950-1980
+average_var = sc.broadcast(average_temps_1950_1980.collectAsMap())
+
+print(average_var.value)
+# take the difference from the average_var values and the values in each (year+month, avg) key-value pair
+temps_differences = average_temps_1950_2014.map(lambda x: (x[0], x[1] - average_var.value[x[0][-2:]]))
+
+# print('temps_diffs', temps_differences.collect()[:10])
+
+temps_differences.saveAsTextFile("ostergotland_avg_temp_diffs")
